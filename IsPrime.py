fee8af2f83fcee03481e60676205ffbda8bff271
Megashyam2K9 <106914156+Megashyam2K9@users.noreply.github.com>
2024-09-06 22:01:05 +0530

    Create IsPrime.py
    Prime Number Checker

---
+++ IsPrime.py
@@ -0,0 +1,11 @@
+numbr = input("Input a number: ")
+num = int(numbr)
+if num > 1:
+    for i in range(2,(num//2)+1):
+        if num % i ==0:
+            Print("Not prime :(")
+            break
+        else:
+            print("is prime")
+            break
+else: print ("enter a proper number!")
